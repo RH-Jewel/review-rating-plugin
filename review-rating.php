968759fef8fd7ad3bab92183db043cdf8d506f93
RH-Jewel <riponhossain876@gmail.com>
2025-10-08 19:39:37 +0600

    core file convert to oop format

--- review-rating.php
+++ review-rating.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: Review & Rating
  * Description: Smart plugin for multi-criteria reviews and ratings to boost engagement.
@@ -14,128 +15,113 @@ if (! defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
 
-// /**
-//  * The main plugin class
-//  */
-// final class Review_Main
-// {
+/**
+ * The main plugin class
+ */
+final class Review_Main
+{
 
-//     /**
-//      * Plugin version
-//      *
-//      * @var string
-//      */
-//     const version = '1.0.0';
+    /**
+     * Plugin version
+     *
+     * @var string
+     */
+    const version = '1.0.0';
 
-//     /**
-//      * Class construcotr
-//      */
-//     private function __construct()
-//     {
-//         $this->define_constants();
-//         $this->require_files();
+    /**
+     * Class construcotr
+     */
+    private function __construct()
+    {
+        $this->define_constants();
+        $this->require_files();
 
+        add_action('wp_enqueue_scripts', [$this,'enqueue_files']);
 
-//         add_action('plugins_loaded', [$this, 'plugin_classes_init']);
-//     }
+        add_action('plugins_loaded', [$this, 'plugin_classes_init']);
+    }
 
-//     /**
-//      * Initializes a singleton instance
-//      *
-//      * @return \Review_Main
-//      */
-//     public static function init()
-//     {
-//         static $instance = false;
+    /**
+     * Initializes a singleton instance
+     *
+     * @return \Review_Main
+     */
+    public static function init()
+    {
+        static $instance = false;
 
-//         if (!$instance) {
-//             $instance = new self();
-//         }
+        if (!$instance) {
+            $instance = new self();
+        }
 
-//         return $instance;
-//     }
+        return $instance;
+    }
 
-//     /**
-//      * Define the required plugin constants
-//      *
-//      * @return void
-//      */
-//     public function define_constants()
-//     {
-//         define('REVIEW_RATING_PATH', plugin_dir_path(__FILE__));
-//         define('REVIEW_RATING_URL', plugin_dir_url(__FILE__));
-//     }
+    /**
+     * Define the required plugin constants
+     *
+     * @return void
+     */
+    public function define_constants()
+    {
+        define('REVIEW_RATING_PATH', plugin_dir_path(__FILE__));
+        define('REVIEW_RATING_URL', plugin_dir_url(__FILE__));
+    }
 
-//     /**
-//      * Include all require files
-//      *
-//      * @return void
-//      */
-//     public function require_files()
-//     {
-//         require_once REVIEW_RATING_PATH . 'includes/class-review-rating-cpt.php';
-//         require_once REVIEW_RATING_PATH . 'includes/class-review-rating-shortcode.php';
-//         require_once REVIEW_RATING_PATH . 'includes/class-review-rating-card-shortcode.php';
-//         require_once REVIEW_RATING_PATH . 'includes/class-review-rating-settings.php';
-//     }
+    /**
+     * Include all require files
+     *
+     * @return void
+     */
+    public function require_files()
+    {
+        require_once REVIEW_RATING_PATH . 'includes/class-review-rating-cpt.php';
+        require_once REVIEW_RATING_PATH . 'includes/class-review-rating-shortcode.php';
+        require_once REVIEW_RATING_PATH . 'includes/class-review-rating-card-shortcode.php';
+        require_once REVIEW_RATING_PATH . 'includes/class-review-rating-settings.php';
+    }
 
-//     /**
-//      * Initialize the plugin
-//      *
-//      * @return void
-//      */
-//     public function plugin_classes_init()
-//     {
-//         new Review_Rating_CPT();
-//         new Review_Rating_Shortcode();
-//         new Post_Rating_Shortcode();
-//         new Review_Rating_Settings();
-//     }
+    /**
+     * Include all require files
+     *
+     * @return void
+     */
+    public function enqueue_files()
+    {
+        // CSS
+        wp_enqueue_style('review-rating-style', REVIEW_RATING_URL . 'assets/css/review-rating.css', [], '1.0.0');
+        // JS
+        wp_enqueue_script('review-rating-script', REVIEW_RATING_URL . 'assets/js/review-rating.js', ['jquery'], '1.0.0', true);
+    }
 
-
-
-
-//     // end main class 
-// }
-
-// /**
-//  * Initializes the main plugin
-//  *
-//  * @return \Review_Main
-//  */
-// function review_core()
-// {
-//     return Review_Main::init();
-// }
-
-// // start doing amazing things
-// review_core();
+    /**
+     * Initialize the plugin
+     *
+     * @return void
+     */
+    public function plugin_classes_init()
+    {
+        new Review_Rating_CPT();
+        new Review_Rating_Shortcode();
+        new Post_Rating_Shortcode();
+        new Review_Rating_Settings();
+    }
 
 
 
-// Define constants
-define('REVIEW_RATING_PATH', plugin_dir_path(__FILE__));
-define('REVIEW_RATING_URL', plugin_dir_url(__FILE__));
 
-// Include class
-require_once REVIEW_RATING_PATH . 'includes/class-review-rating-cpt.php';
-require_once REVIEW_RATING_PATH . 'includes/class-review-rating-shortcode.php';
-require_once REVIEW_RATING_PATH . 'includes/class-review-rating-card-shortcode.php';
-require_once REVIEW_RATING_PATH . 'includes/class-review-rating-settings.php';
+    // end main class 
+}
 
-// Enqueue plugin assets
-add_action('wp_enqueue_scripts', function () {
-    // CSS
-    wp_enqueue_style('review-rating-style', REVIEW_RATING_URL . 'assets/css/review-rating.css', [], '1.0.0');
-    // JS
-    wp_enqueue_script('review-rating-script', REVIEW_RATING_URL . 'assets/js/review-rating.js', ['jquery'], '1.0.0', true);
-});
+/**
+ * Initializes the main plugin
+ *
+ * @return \Review_Main
+ */
+function review_core()
+{
+    return Review_Main::init();
+}
 
-
-// Initialize
-add_action('plugins_loaded', function () {
-    new Review_Rating_CPT();
-    new Review_Rating_Shortcode();
-    new Post_Rating_Shortcode();
-    new Review_Rating_Settings();
-});
+// start doing amazing things
+review_core();
